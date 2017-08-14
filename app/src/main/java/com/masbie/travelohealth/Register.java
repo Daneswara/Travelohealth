63367e78e6d8b79a51ad9ac4fab9ff57ebfd20a3
Daneswara <daneswarajauhari@gmail.com>
2017-08-14 21:14:55 +0700

    desain daftar

--- app/src/main/java/com/masbie/travelohealth/Register.java
+++ app/src/main/java/com/masbie/travelohealth/Register.java
@@ -1,13 +1,59 @@
 package com.masbie.travelohealth;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Register extends AppCompatActivity {
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 
+import java.util.Calendar;
+
+public class Register extends AppCompatActivity implements com.wdullaer.materialdatetimepicker.date.DatePickerDialog.OnDateSetListener{
+    EditText nama, alamat, ttl, password;
+    Button daftar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        nama = (EditText) findViewById(R.id.nama);
+        alamat = (EditText) findViewById(R.id.alamat);
+        ttl = (EditText) findViewById(R.id.tgllahir);
+        password = (EditText) findViewById(R.id.password);
+        daftar = (Button) findViewById(R.id.daftar);
+        ttl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar now = Calendar.getInstance();
+                com.wdullaer.materialdatetimepicker.date.DatePickerDialog dpd = com.wdullaer.materialdatetimepicker.date.DatePickerDialog.newInstance(
+                        Register.this,
+                        now.get(Calendar.YEAR),
+                        now.get(Calendar.MONTH),
+                        now.get(Calendar.DAY_OF_MONTH)
+                );
+                dpd.setAccentColor(getResources().getColor(R.color.colorPrimary));
+                dpd.setMaxDate(now);
+                dpd.setOkText("Pilih");
+                dpd.setCancelText("Batal");
+                dpd.show(getFragmentManager(), "Datepickerdialog");
+            }
+        });
+
+        daftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        String date = String.format("%02d", dayOfMonth) + "-" + String.format("%02d", (monthOfYear + 1)) + "-" + year;
+        ttl.setText("Tanggal: " + date);
     }
 }
