3f4cd985047805ec9a07e1f51a12c98508b2a93c
Daneswara <daneswarajauhari@gmail.com>
2017-10-19 19:13:17 +0700

    update FCM

--- app/src/main/java/com/masbie/travelohealth/MyFirebaseMessagingService.java
+++ app/src/main/java/com/masbie/travelohealth/MyFirebaseMessagingService.java
@@ -9,6 +9,7 @@ import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -18,6 +19,7 @@ import com.google.firebase.messaging.RemoteMessage;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String TAG = "MyFirebaseMsgService";
+    static int notifId = -1;
 
     /**
      * Called when message is received.
@@ -37,43 +39,54 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
         // [END_EXCLUDE]
 
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
 
-        // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+            String topic = remoteMessage.getFrom().replace("/topics/", "");
 
-        }
+            String pesanan = remoteMessage.getData().get("pesanan");
+            if(pesanan.equals("service")) {
+                String status = remoteMessage.getData().get("status");
+                if(status.equals("antrian")) {
+                    String antrian = remoteMessage.getData().get("antrian");
+                    sendNotification("Travelohealth - " + topic, "Sekarang antrian ke-" + antrian);
+                } else if(status.equals("proses")){
+                    sendNotification("Travelohealth - " + topic, "Sekarang nomer antrian anda");
+                    FirebaseMessaging.getInstance().unsubscribeFromTopic(topic);
+                }
 
-        // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-            sendNotification(remoteMessage.getNotification().getBody());
+            } else if(pesanan.equals("room")) {
+                String validasi = remoteMessage.getData().get("validasi");
+                if(validasi.equals("1")){
+                    String nomerkamar = remoteMessage.getData().get("nomerkamar");
+                    sendNotification("Travelohealth", "Selamat validasi anda berhasil, anda bisa menempati kamar "+nomerkamar);
+                } else {
+                    sendNotification("Travelohealth", "Mohon maaf validasi pesanan anda gagal");
+                }
+
+            }
         }
 
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
     }
     // [END receive_message]
+
     /**
      * Create and show a simple notification containing the received FCM message.
      *
      * @param messageBody FCM message body received.
      */
-    private void sendNotification(String messageBody) {
+    private void sendNotification(String title, String messageBody) {
         Intent intent = new Intent(this, Home.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
-        String channelId = getString(R.string.default_notification_channel_id);
-        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder =
-                new NotificationCompat.Builder(this, channelId)
-                        .setSmallIcon(R.drawable.ic_notifications)
-                        .setContentTitle("Travelohealth")
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle(title)
                         .setContentText(messageBody)
                         .setAutoCancel(true)
                         .setSound(defaultSoundUri)
@@ -82,6 +95,6 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        notificationManager.notify(++MyFirebaseMessagingService.notifId, notificationBuilder.build());
     }
 }
