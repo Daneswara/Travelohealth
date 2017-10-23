e244f56783abc91b92b2075758fa87149a96c2fb
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-10-23 16:09:11 +0700

    Add Application activitiy

---
+++ app/src/main/java/com/masbie/travelohealth/App.java
@@ -0,0 +1,60 @@
+package com.masbie.travelohealth;
+
+/*
+ * This <client-android-travelohealth> created by : 
+ * Name         : syafiq
+ * Date / Time  : 25 August 2017, 12:17 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import net.danlew.android.joda.JodaTimeAndroid;
+import timber.log.Timber;
+
+public class App extends Application
+{
+    @Override public void onCreate()
+    {
+        super.onCreate();
+        this.initializeTimber();
+        this.initializeJoda();
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            // Create channel to show notifications.
+            String channelId = getString(R.string.default_notification_channel_id);
+            String channelName = getString(R.string.default_notification_channel_name);
+            NotificationManager notificationManager =
+                    getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(new NotificationChannel(channelId,
+                    channelName, NotificationManager.IMPORTANCE_LOW));
+        }
+    }
+
+    @Override
+    protected void attachBaseContext(Context base)
+    {
+        super.attachBaseContext(base);
+
+        // The following line triggers the initialization of ACRA
+    }
+
+    private void initializeJoda()
+    {
+        JodaTimeAndroid.init(this);
+    }
+
+    private void initializeTimber()
+    {
+        if(BuildConfig.DEBUG)
+        {
+            Timber.plant(new Timber.DebugTree());
+        }
+        Timber.d("initializeTimber");
+    }
+}
