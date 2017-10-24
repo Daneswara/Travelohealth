96b8e9b36b18b9e3993adfe8ba1b155716c5f4ad
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-10-24 10:09:05 +0700

    Add some pojo

---
+++ app/src/main/java/com/masbie/travelohealth/pojo/service/DetailedRoomClassPojo.java
@@ -0,0 +1,104 @@
+package com.masbie.travelohealth.pojo.service;
+
+/*
+ * This <client-android-travelohealth> created by : 
+ * Name         : syafiq
+ * Date / Time  : 03 September 2017, 3:04 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.SerializedName;
+
+public class DetailedRoomClassPojo extends RoomClassPojo
+{
+    @SerializedName("capacity")
+    @Nullable private Integer capacity;
+    @SerializedName("ready")
+    @Nullable private Integer ready;
+
+    public DetailedRoomClassPojo(@Nullable Integer capacity, @Nullable Integer ready)
+    {
+        this.capacity = capacity;
+        this.ready = ready;
+    }
+
+    public DetailedRoomClassPojo(@Nullable Integer id, @Nullable String name, @Nullable Double cost, @Nullable String feature, @Nullable String preview, Integer capacity, Integer ready)
+    {
+        super(id, name, cost, feature, preview);
+        this.capacity = capacity;
+        this.ready = ready;
+    }
+
+    public static void inferenceGsonBuilder(@NonNull GsonBuilder builder)
+    {
+        RoomClassPojo.inferenceGsonBuilder(builder);
+    }
+
+    @Nullable public Integer getCapacity()
+    {
+        return this.capacity;
+    }
+
+    public void setCapacity(@Nullable Integer capacity)
+    {
+        this.capacity = capacity;
+    }
+
+    @Nullable public Integer getReady()
+    {
+        return this.ready;
+    }
+
+    public void setReady(@Nullable Integer ready)
+    {
+        this.ready = ready;
+    }
+
+    @SuppressWarnings("SimplifiableIfStatement") @Override public boolean equals(Object o)
+    {
+        if(this == o)
+        {
+            return true;
+        }
+        if(!(o instanceof DetailedRoomClassPojo))
+        {
+            return false;
+        }
+        if(!super.equals(o))
+        {
+            return false;
+        }
+
+        DetailedRoomClassPojo that = (DetailedRoomClassPojo) o;
+
+        if(getCapacity() != null ? !getCapacity().equals(that.getCapacity()) : that.getCapacity() != null)
+        {
+            return false;
+        }
+        return getReady() != null ? getReady().equals(that.getReady()) : that.getReady() == null;
+
+    }
+
+    @Override public int hashCode()
+    {
+        int result = super.hashCode();
+        result = 31 * result + (getCapacity() != null ? getCapacity().hashCode() : 0);
+        result = 31 * result + (getReady() != null ? getReady().hashCode() : 0);
+        return result;
+    }
+
+    @Override public String toString()
+    {
+        return "DetailedRoomClassPojo{" + "id=" + super.getId() +
+                ", name='" + super.getName() + '\'' +
+                ", cost=" + super.getCost() +
+                ", feature='" + super.getFeature() + '\'' +
+                ", capacity=" + capacity +
+                ", ready=" + ready +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/masbie/travelohealth/pojo/service/RoomSectorPojo.java
@@ -0,0 +1,84 @@
+package com.masbie.travelohealth.pojo.service;
+
+/*
+ * This <client-android-travelohealth> created by : 
+ * Name         : syafiq
+ * Date / Time  : 03 September 2017, 3:00 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.SerializedName;
+import java.util.List;
+
+public class RoomSectorPojo<T extends RoomClassPojo> extends RoomPojo
+{
+    @SerializedName("classes")
+    @Nullable List<T> classes;
+
+    public RoomSectorPojo(@Nullable List<T> classes)
+    {
+        this.classes = classes;
+    }
+
+    public RoomSectorPojo(@Nullable Integer id, @Nullable String name, @Nullable List<T> classes)
+    {
+        super(id, name);
+        this.classes = classes;
+    }
+
+    public static void inferenceGsonBuilder(@NonNull GsonBuilder builder)
+    {
+        RoomClassPojo.inferenceGsonBuilder(builder);
+        RoomPojo.inferenceGsonBuilder(builder);
+    }
+
+    @Nullable public List<T> getClasses()
+    {
+        return this.classes;
+    }
+
+    public void setClasses(@Nullable List<T> classes)
+    {
+        this.classes = classes;
+    }
+
+    @Override public boolean equals(Object o)
+    {
+        if(this == o)
+        {
+            return true;
+        }
+        if(!(o instanceof RoomSectorPojo))
+        {
+            return false;
+        }
+        if(!super.equals(o))
+        {
+            return false;
+        }
+
+        RoomSectorPojo<?> that = (RoomSectorPojo<?>) o;
+
+        return classes != null ? classes.equals(that.classes) : that.classes == null;
+
+    }
+
+    @Override public int hashCode()
+    {
+        int result = super.hashCode();
+        result = 31 * result + (classes != null ? classes.hashCode() : 0);
+        return result;
+    }
+
+    @Override public String toString()
+    {
+        return "RoomSectorPojo{" + "id=" + super.getId() +
+                ", name='" + super.getName() + '\'' +
+                ", classes=" + classes +
+                '}';
+    }
+}
